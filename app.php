f161b4a28fcdffccc6f222029fc0cce986e3bd0d
Melanie Macken <m.macken@otmail.nl>
2018-02-13 18:45:26 +0100

    Contact form

--- app.php
+++ app.php
@@ -524,52 +524,51 @@
             <div class="col">
                 <!-- <h4>Where to find us and Contact</h4> -->
 
-                <form>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="name">
-                                    Name</label>
-                                <input type="text" class="form-control" id="name" placeholder="Enter name"
-                                       required="required"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="email">
-                                    Email Address</label>
-                                <div class="input-group">
-                                <span class="input-group-addon">
-                                    <span class="glyphicon glyphicon-envelope"></span>
-                                </span>
-                                    <input type="email" class="form-control" id="email" placeholder="Enter email"
-                                           required="required"/>
+                    <form action="form.php" method="post">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="name">
+                                        Subject</label>
+                                    <input type="text" name="subject" class="form-control" id="name" placeholder="Enter name" required="required" />
+                                </div>
+                                <div class="form-group">
+                                    <label for="name">
+                                        Name</label>
+                                    <input type="text" name="name" class="form-control" id="name" placeholder="Enter name" required="required" />
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">
+                                        Email Address</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon">
+                                            <span class="glyphicon glyphicon-envelope"></span>
+                                        </span>
+                                        <input type="text" name="email" class="form-control" id="email" placeholder="Enter email" required="required" />
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                    
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label for="subject">
-                                    Subject</label>
-                                <select id="subject" name="subject" class="form-control" required="required">
-                                    <option value="na" selected="">Choose One:</option>
-                                    <option value="service">Questions</option>
-                                    <option value="suggestions">Suggestions</option>
-                                    <option value="product">Bookings</option>
-                                </select>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="name">
+                                        Message</label>
+                                    <textarea type="text" name="message" id="message" class="form-control" rows="9" cols="25" required="required" placeholder="Message"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <button type="submit" name "submit" class="btn btn-primary pull-right" id="btnContactUs">
+                                    Send Message
+                                </button>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="name">
-                                    Message</label>
-                                <textarea name="message" id="message" class="form-control" rows="9" cols="25"
-                                          required="required" placeholder="Message"></textarea>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
-                                Send Message
-                            </button>
-                        </div>
+                    </form>
+                    
                     </div>
-                </form>
+                    </div>
+
 
             </div>
         </div>
