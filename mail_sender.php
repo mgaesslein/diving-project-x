cec1d42a587ae93412cf3789dd16b1f89dccda70
Maximlian Gäßlein <maximilian.gaesslein@scandio.de>
2018-02-17 21:54:07 +0100

    fixed the form and removed some unnecessary code

--- mail_sender.php
+++ mail_sender.php
@@ -5,28 +5,13 @@ use PHPMailer\PHPMailer\Exception;
 require 'lib/PHPMailer/src/Exception.php';
 require 'lib/PHPMailer/src/PHPMailer.php';
 require 'lib/PHPMailer/src/SMTP.php';
-
+print_r($_POST);
 if (isset($_POST['email'])) {
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "aljaz.gantar94@gmail.com";
     $email_subject = "Something random";
-    function died($error)
-    {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error . "<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
-
-
-    // validation expected data exists
-    if (!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');
-    }
+    
+    echo 'test';
 
     $name = $_POST['name']; // required
     $email_from = $_POST['email']; // required
@@ -50,7 +35,7 @@ if (isset($_POST['email'])) {
     }
 
     if (strlen($error_message) > 0) {
-        died($error_message);
+        die($error_message);
     }
 
     $email_message = "Form details below.\n\n";
@@ -66,10 +51,6 @@ if (isset($_POST['email'])) {
     $email_message .= "Email: " . clean_string($email_from) . "\n";
     $email_message .= "message: " . clean_string($message) . "\n";
 
-// create email headers
-    $headers = 'From: ' . $email_from . "\r\n" .
-        'Reply-To: ' . $email_from . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
 
     $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
     try {
@@ -80,8 +61,8 @@ if (isset($_POST['email'])) {
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
         $mail->Username = 'test@gaesslein.net';                 // SMTP username
         $mail->Password = 'abcdefg123';                           // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 587;                                    // TCP port to connect to
+        // $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 25;                                    // TCP port to connect to
 
         //Recipients
         $mail->setFrom('test@gaesslein.net', 'Mailer');
